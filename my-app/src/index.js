71363ed7f638c04c673eaf125c41b82e69f67627
angelica <mariaanguevara@gmail.com>
2020-02-07 16:24:38 -0500

    fix: package.json

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,13 @@
-
-
+import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import "babel-polyfill";
-// import 'whatwg-fetch';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
